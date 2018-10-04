016e5f735577bd86edb68939867b3cb4aff6a3d0
VictorCarreira <carreiravr@gmail.com>
2018-10-04 18:54:04 -0300

    adiciona módulos e a função sinal

---
+++ perceptron_mod.f95
@@ -0,0 +1,58 @@
+MODULE perceptron_mod
+IMPLICIT NONE
+  PUBLIC
+  INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=8)
+  INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(12,100)
+  INTEGER(KIND=SP):: n
+  REAL(KIND=DP)::inicio,final, TM, c
+  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:):: x, y
+  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:):: A, bt, xt
+
+  
+CONTAINS
+
+REAL FUNCTION Signal(x,y)
+ 
+REAL(KIND=DP), INTENT(IN):: x
+REAL(KIND=DP), INTENT(OUT):: y 
+
+  IF(x >= 0.0 ) THEN
+   y = -1   
+  ELSE IF (x < 0.0) THEN
+   y = +1 
+  END IF
+
+END FUNCTION Signal
+
+ SUBROUTINE transpostaM(n,A)
+  IMPLICIT NONE
+  INTEGER(KIND=SP), INTENT(IN):: n  !n, dimensão da matriz
+  INTEGER(KIND=SP):: i, j
+  REAL(KIND=DP), DIMENSION(:,:), ALLOCATABLE, INTENT(OUT):: A !A é a matriz de entrada. Ela vai ser reescrita no processo
+
+  ALLOCATE(A(n,n))
+
+    DO i=1,n
+      DO j=1,n
+        A(j,i) = A(i,j)
+      ENDDO
+    ENDDO
+
+END SUBROUTINE transpostaM
+
+SUBROUTINE transpostaV(n,At)
+IMPLICIT NONE
+INTEGER(KIND=SP), INTENT(IN):: n  !n, dimensão da matriz
+INTEGER(KIND=SP):: i
+REAL(KIND=DP), DIMENSION(:,:), ALLOCATABLE, INTENT(OUT):: At !A é a matriz de entrada. Ela vai ser reescrita no processo
+REAL(KIND=DP), DIMENSION(:,:), ALLOCATABLE:: A
+
+ALLOCATE(A(1,n),At(n,1))
+
+  DO i=1,n
+      At(:,i) = A(i,:)
+  ENDDO
+
+END SUBROUTINE transpostaV
+
+END MODULE perceptron_mod
