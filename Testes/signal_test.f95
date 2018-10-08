ec5fdaedf7d28ef164c21441e6dfad70e641c73b
VictorCarreira <carreiravr@gmail.com>
2018-10-08 18:37:15 -0300

    Conserta a matriz w

--- Testes/signal_test.f95
+++ Testes/signal_test.f95
@@ -1,12 +1,12 @@
 PROGRAM signal_test
  USE mod  
  IMPLICIT NONE
-  INTEGER, PARAMETER::SSP = SELECTED_INT_KIND(r=8)
+  !INTEGER, PARAMETER::SSP = SELECTED_INT_KIND(r=8)
   INTEGER, PARAMETER::DDP = SELECTED_REAL_KIND(12,100)
   REAL(KIND=DDP):: aa, bb
   REAL(KIND=DDP), ALLOCATABLE, DIMENSION(:,:):: xx, w, wt   
 
-  ALLOCATE(wt(1,3),w(3,1),xx(3,1))
+  ALLOCATE(wt(3,3),w(3,3),xx(3,3))
   
   !teste de validação. 
 
@@ -20,9 +20,9 @@ PROGRAM signal_test
 
   CALL transpostaV(3,w)
   
-  PRINT*, w
+  aa = dot_product(w,xx)
   
-  !aa = dot_product(wt,xx)
+  PRINT*,aa
 
   !CALL Signal(aa,bb) 
   
