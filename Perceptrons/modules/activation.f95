dc2fc0e50108e89b06abe08e5d6dc8bf690b641b
VictorCarreira <carreiravr@gmail.com>
2018-11-28 01:56:09 -0200

    Adiciona novas condições de fronteira em synaptic

--- Perceptrons/modules/activation.f95
+++ Perceptrons/modules/activation.f95
@@ -3,9 +3,10 @@ IMPLICIT NONE
   PUBLIC
   INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=4)
   INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(8,10)
+  INTEGER(KIND=SP):: epoch  
   REAL(KIND=DP), ALLOCATABLE, DIMENSION(:)::pp
-  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:):: xxi, w
-  REAL(KIND=DP):: x, aa, n, eta
+  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:):: xxi,xxi1,xxi2, w
+  REAL(KIND=DP):: x, aa, n, eta, a1, a2, SC1, SC2
 
   
 CONTAINS
@@ -55,31 +56,36 @@ END SUBROUTINE treinamento
 
 
 
-SUBROUTINE synaptic(xxi1,xxi2,a1,a2,eta,epoch,w)
+SUBROUTINE synaptic(xxi1,xxi2,a1,a2,SC1,SC2,eta,epoch,w)
 IMPLICIT NONE
 INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=4)
 INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(8,10)
 REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:), INTENT(IN):: xxi1,xxi2
 REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:), INTENT(INOUT):: w
-REAL(KIND=DP), INTENT(IN):: a1, a2, eta
-INTEGER(KIND=DP), INTENT(IN):: epoch
+REAL(KIND=DP), INTENT(IN):: a1, a2, SC1, SC2, eta
+INTEGER(KIND=SP), INTENT(IN):: epoch
 INTEGER(KIND=SP):: i,j,k
 
 DO k=1,epoch !épocas
- DO j=1,8 !número de linhas da matriz de entrada
-  DO i=1,4 ! número de propriedades da matriz de entrada 
-    IF(a1 <= 0d0 .and. a2 < 0d0) THEN 
-      w(i,1)=w(i,1)
-    ELSE IF(a1 >= 0d0 .and. a2 > 0d0) THEN
-      w(i,1)=w(i,1)+eta*xxi1(j,i)
-      w(i,1)=w(i,1)-eta*xxi2(j,i) 
+ DO i=1,8 !número de linhas da matriz de entrada
+  DO j=1,4 ! número de propriedades da matriz de entrada 
+    IF(a1 < 0d0 .or. SC1 >= 0d0)THEN 
+     w(i,1)=w(i,1)+eta*xxi1(i,j) 
+    ELSE
+     w(i,1)=w(i,1)  
+    END IF
+
+   IF(a2 >= 0d0 .or. SC2 < 0d0)THEN
+      w(i,1)=w(i,1)-eta*xxi2(i,j) 
+    ELSE
+     w(i,1)=w(i,1)  
     END IF
   END DO 
  END DO
 END DO  
 
-
 END SUBROUTINE synaptic
+
 
 
 END MODULE activation
