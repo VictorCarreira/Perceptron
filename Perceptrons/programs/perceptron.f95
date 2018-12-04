e30d553c3de59b5db78824baaaf4a9b03b59945d
VictorCarreira <carreiravr@gmail.com>
2018-12-04 00:50:30 -0200

    Adiciona

--- Perceptrons/programs/perceptron.f95
+++ Perceptrons/programs/perceptron.f95
@@ -10,16 +10,16 @@ PROGRAM perceptron
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MODULOS UTILIZADOS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MODULOS UTILIZADOS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 USE activation
 USE algebra
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DECLARAÇÃO DAS VARIÁVEIS GLOBAIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DECLARAÇÃO DAS VARIÁVEIS GLOBAIS !!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 IMPLICIT NONE 
   INTEGER, PARAMETER::SSP = SELECTED_INT_KIND(r=4)
@@ -34,7 +34,8 @@ IMPLICIT NONE
 
   ALLOCATE(xi1(8,4),xi2(8,4),csi(12,4),omega(4,1))
 
-  OPEN(1,FILE='outputs/saida.txt')
+  OPEN(1,FILE='inputs/folhelho.txt')
+  OPEN(2,FILE='outputs/saida.txt')
    
 
   CALL cpu_time(vi) 
@@ -46,111 +47,112 @@ IMPLICIT NONE
   !epoca=0  !Número de ciclos de treinamento
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ENTRADA DA REDE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FASE DE TREINAMENTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ENTRADA DA REDE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FASE DE TREINAMENTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+
+  CALL entrada()
 
  !Vetor pesos:
 
   CALL pesos(4,1,1.0d00,omega)
-
  
 !Sinal de entrada para treinamento classe 1 (Folhelho)
   
   !Amazonas (Colunas:densidade,gama,resistividade,velocidade)
   !Sub-classe 1 folhelho
   !                                _   
-  xi1(1,1)=2.57d00  !densidade      |
-  xi1(1,2)=9.69d01  !gama           |==> primeira linha da matriz xi1
-  xi1(1,3)=9.29d03  !resistividade  |
-  xi1(1,4)=2.87d00  !velocidade    _|
+  !xi1(1,1)=2.57d00  !densidade      |
+  !xi1(1,2)=9.69d01  !gama           |==> primeira linha da matriz xi1
+  !xi1(1,3)=9.29d03  !resistividade  |
+  !xi1(1,4)=2.87d00  !velocidade    _|
   !                                _
-  xi1(2,1)=2.52d00  !densidade      |
-  xi1(2,2)=1.05d02  !gama           |==> segunda linha da matriz xi1
-  xi1(2,3)=1.02d04  !resistividade  |
-  xi1(2,4)=2.84d00  !velocidade    _|
+  !xi1(2,1)=2.52d00  !densidade      |
+  !xi1(2,2)=1.05d02  !gama           |==> segunda linha da matriz xi1
+  !xi1(2,3)=1.02d04  !resistividade  |
+  !xi1(2,4)=2.84d00  !velocidade    _|
   !                                _
-  xi1(3,1)=2.55d00  !dendidade      |
-  xi1(3,2)=9.51d01  !gama           |==> terceira linha da matriz xi1 
-  xi1(3,3)=9.93d03  !resistividade  | 
-  xi1(3,4)=2.96d00  !velocidade    _|
+  !xi1(3,1)=2.55d00  !dendidade      |
+  !xi1(3,2)=9.51d01  !gama           |==> terceira linha da matriz xi1 
+  !xi1(3,3)=9.93d03  !resistividade  | 
+  !xi1(3,4)=2.96d00  !velocidade    _|
   !                                _
-  xi1(4,1)=2.53d00  !densidade      |
-  xi1(4,2)=9.71d01  !gama           |==> quarta linha da matriz xi1
-  xi1(4,3)=9.93d03  !resistividade  |
-  xi1(4,4)=3.15d00  !velocidade    _|
+  !xi1(4,1)=2.53d00  !densidade      |
+  !xi1(4,2)=9.71d01  !gama           |==> quarta linha da matriz xi1
+  !xi1(4,3)=9.93d03  !resistividade  |
+  !xi1(4,4)=3.15d00  !velocidade    _|
   !                                _
-  xi1(5,1)=2.58d00  !densidade      |
-  xi1(5,2)=9.49d01  !gama           |==> quinta linha da matriz xi1
-  xi1(5,3)=1.03d04  !resistividade  |
-  xi1(5,4)=2.96d00  !velocidade    _!
+  !xi1(5,1)=2.58d00  !densidade      |
+  !xi1(5,2)=9.49d01  !gama           |==> quinta linha da matriz xi1
+  !xi1(5,3)=1.03d04  !resistividade  |
+  !xi1(5,4)=2.96d00  !velocidade    _!
   !                                _
-  xi1(6,1)=2.51d00  !densidade      |
-  xi1(6,2)=9.94d01  !gama           |==> sexta linha da matriz xi1
-  xi1(6,3)=9.94d03  !resistividade  |
-  xi1(6,4)=2.83d00  !velocidade    _|
+  !xi1(6,1)=2.51d00  !densidade      |
+  !xi1(6,2)=9.94d01  !gama           |==> sexta linha da matriz xi1
+  !xi1(6,3)=9.94d03  !resistividade  |
+  !xi1(6,4)=2.83d00  !velocidade    _|
   !                                _
-  xi1(7,1)=2.51d00  !densidade      |
-  xi1(7,2)=1.06d02  !gama           |==> sétima linha da matriz xi1
-  xi1(7,3)=1.04d04  !resistivade    |
-  xi1(7,4)=3.01d00  !velocidade    _|
+  !xi1(7,1)=2.51d00  !densidade      |
+  !xi1(7,2)=1.06d02  !gama           |==> sétima linha da matriz xi1
+  !xi1(7,3)=1.04d04  !resistivade    |
+  !xi1(7,4)=3.01d00  !velocidade    _|
   !                                _
-  xi1(8,1)=2.58d00  !densidade      |
-  xi1(8,2)=1.07d02  !gama           |==> oitava linha da matriz xi1
-  xi1(8,3)=1.01d04  !resistivade    |
-  xi1(8,4)=3.14d00  !velocidade    _|
+  !xi1(8,1)=2.58d00  !densidade      |
+  !xi1(8,2)=1.07d02  !gama           |==> oitava linha da matriz xi1
+  !xi1(8,3)=1.01d04  !resistivade    |
+  !xi1(8,4)=3.14d00  !velocidade    _|
 
 !Sinal de entrada para treinamento sub-classe 2 (multi-rochas)
 !                                  _
-  xi2(1,1)=2.70d00  !densidade      |
-  xi2(1,2)=8.16d00  !gama           |===> primeira linha da matriz xi2 (dolomita)
-  xi2(1,3)=3.54d03  !resistividade  |
-  xi2(1,4)=6.39d00  !velocidade    _|
+  !xi2(1,1)=2.70d00  !densidade      |
+  !xi2(1,2)=8.16d00  !gama           |===> primeira linha da matriz xi2 (dolomita)
+  !xi2(1,3)=3.54d03  !resistividade  |
+  !xi2(1,4)=6.39d00  !velocidade    _|
 !                                  _
-  xi2(2,1)=2.92d00  !densidade      |
-  xi2(2,2)=3.07d01  !gama           |===> segunda linha da matriz xi2 (diabasio)
-  xi2(2,3)=1.58d08  !resistividade  |
-  xi2(2,4)=5.56d00  !velocidade    _|
+  !xi2(2,1)=2.92d00  !densidade      |
+  !xi2(2,2)=3.07d01  !gama           |===> segunda linha da matriz xi2 (diabasio)
+  !xi2(2,3)=1.58d08  !resistividade  |
+  !xi2(2,4)=5.56d00  !velocidade    _|
 !                                  _                    
-  xi2(3,1)=2.30d00  !densidade      |
-  xi2(3,2)=9.45d01  !gama           |===> terceira linha da matiz xi2 (conglomerado)
-  xi2(3,3)=5.85d03  !resistividade  |
-  xi2(3,4)=1.99d00  !velocidade    _|
+  !xi2(3,1)=2.30d00  !densidade      |
+  !xi2(3,2)=9.45d01  !gama           |===> terceira linha da matiz xi2 (conglomerado)
+  !xi2(3,3)=5.85d03  !resistividade  |
+  !xi2(3,4)=1.99d00  !velocidade    _|
 !                                  _
-  xi2(4,1)=2.83d00  !densidade      |
-  xi2(4,2)=6.86d-01 !gama           |===> quarta linha da matriz xi2 (embasamento)
-  xi2(4,3)=1.40d06  !resistividade  |
-  xi2(4,4)=5.26d00  !velocidade    _|
+  !xi2(4,1)=2.83d00  !densidade      |
+  !xi2(4,2)=6.86d-01 !gama           |===> quarta linha da matriz xi2 (embasamento)
+  !xi2(4,3)=1.40d06  !resistividade  |
+  !xi2(4,4)=5.26d00  !velocidade    _|
 !                                  _
-  xi2(5,1)=2.26d00  !densidade      | 
-  xi2(5,2)=9.32d01  !gama           |===> quinta linha da matriz xi2 (cong+emb1)
-  xi2(5,3)=1.26d04  !resistividade  |
-  xi2(5,4)=2.10d00  !velocidade    _|
+  !xi2(5,1)=2.26d00  !densidade      | 
+  !xi2(5,2)=9.32d01  !gama           |===> quinta linha da matriz xi2 (cong+emb1)
+  !xi2(5,3)=1.26d04  !resistividade  |
+  !xi2(5,4)=2.10d00  !velocidade    _|
 !                                  _
-  xi2(6,1)=2.47d00  !densidade      |
-  xi2(6,2)=7.36d01  !gama           |===> sexta linha da matriz xi2 (cong+emb2)
-  xi2(6,3)=3.00d05  !resistividade  |
-  xi2(6,4)=2.60d00  !velocidade    _|
+  !xi2(6,1)=2.47d00  !densidade      |
+  !xi2(6,2)=7.36d01  !gama           |===> sexta linha da matriz xi2 (cong+emb2)
+  !xi2(6,3)=3.00d05  !resistividade  |
+  !xi2(6,4)=2.60d00  !velocidade    _|
 !                                  _
-  xi2(7,1)=2.53d00  !densidade      |
-  xi2(7,2)=5.33d01  !gama           |===> sétima linha da matriz xi2 (cong+emb3)
-  xi2(7,3)=6.11d05  !resisitividade |
-  xi2(7,4)=3.27d00  !velocidade    _|              
+  !xi2(7,1)=2.53d00  !densidade      |
+  !xi2(7,2)=5.33d01  !gama           |===> sétima linha da matriz xi2 (cong+emb3)
+  !xi2(7,3)=6.11d05  !resisitividade |
+  !xi2(7,4)=3.27d00  !velocidade    _|              
 !                                  _
-  xi2(8,1)=2.66d00  !densidade      | 
-  xi2(8,2)=2.42d01  !gama           |===> oitava linha da matriz xi2 (cong+emb4)
-  xi2(8,3)=1.06d06  !resistividade  | 
-  xi2(8,4)=4.14d00  !velocidade    _|
+  !xi2(8,1)=2.66d00  !densidade      | 
+  !xi2(8,2)=2.42d01  !gama           |===> oitava linha da matriz xi2 (cong+emb4)
+  !xi2(8,3)=1.06d06  !resistividade  | 
+  !xi2(8,4)=4.14d00  !velocidade    _|
  
 
 !Número de épocas
   epoca = 2009 !numero ótimo para o folhelho
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TREINAMENTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TREINAMENTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !************************************************!
 !******* Definindo conjunto de treinamento ******!
@@ -198,10 +200,10 @@ CALL synaptic(xi1,xi2,etaD,omega) !Com critério de parada. Calcula o número de
   WRITE(*,FMT=14)omega
     
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Fase de Classificação !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! vetor W atualizado !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Fase de Classificação !!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! vetor W atualizado !!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 !Input (Conjunto de classificação)
