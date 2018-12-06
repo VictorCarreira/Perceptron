0d93f05dcd8cc133fa39deb9c60d688358137209
VictorCarreira <carreiravr@gmail.com>
2018-12-06 19:04:15 -0200

    Automatiza os dados de entrada da fase de classificação

--- Perceptrons/programs/perceptron.f95
+++ Perceptrons/programs/perceptron.f95
@@ -1,8 +1,8 @@
 PROGRAM perceptron
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
-  !Criação de um Perceptron simples para análise de uma litologia              !
-  !Aluno: Victor Ribeiro Carreira                                              !
+  !Criação de um Perceptron para análise de uma litologia                      !
+  !Criador: Victor Ribeiro Carreira                                            !
   !Este programa visa simular um perceptron de Hosenblat                       !
   !Para usar compilação com flags utilize: make                                !
   !Para usar o comando de limpeza digite: make clean                           !
@@ -22,17 +22,17 @@ USE algebra
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 IMPLICIT NONE 
-  INTEGER, PARAMETER::SSP = SELECTED_INT_KIND(r=4)
+  INTEGER, PARAMETER::SSP = SELECTED_INT_KIND(r=8)
   INTEGER, PARAMETER::DDP = SELECTED_REAL_KIND(8,10)
   INTEGER(KIND=DDP)::epoca 
-  INTEGER(KIND=SSP)::rock
+  INTEGER(KIND=SSP)::rock, nd
   REAL(KIND=DDP):: etaR, etaD, vi, vf, dltv
   REAL(KIND=DDP),PARAMETER::cc=0.1d0, eetaO=20.d0, taau=30.d0
   REAL(KIND=DDP), ALLOCATABLE, DIMENSION(:,:):: xi1, xi2, csi, omega
   CHARACTER(LEN=30):: evaluation
 
 
-  ALLOCATE(csi(12,4),omega(4,1))
+  ALLOCATE(omega(4,1))
 
   OPEN(1,FILE='inputs/folhelho.txt')
   OPEN(2,FILE='outputs/saida.txt')
@@ -51,7 +51,7 @@ IMPLICIT NONE
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-  CALL E-training(xi1, xi2)
+  CALL Etraining(xi1, xi2)
 
  !Vetor pesos:
 
@@ -88,7 +88,7 @@ IMPLICIT NONE
   WRITE(*,*)'Taxa de aprendizado de Darken'
   WRITE(*,FMT=12)etaD
   
-  WRITE(*,*)'Vetor W'
+  WRITE(*,*)'Vetor w'
   WRITE(*,FMT=14)omega
 
 
@@ -104,7 +104,7 @@ CALL synaptic(xi1,xi2,etaD,omega) !Com critério de parada. Calcula o número de
 
 
 
-  WRITE(*,*)'Vetor W atualizado'
+  WRITE(*,*)'Vetor w atualizado'
   WRITE(*,FMT=14)omega
     
 
@@ -116,63 +116,8 @@ CALL synaptic(xi1,xi2,etaD,omega) !Com critério de parada. Calcula o número de
 
 !Input (Conjunto de classificação)
 
-csi=0.0d00
 
-
-!                                  _
-  csi(1,1)=2.47d00  !densidade      | 
-  csi(1,2)=1.02d02  !gama           |===> primeira linha da matriz csi (folhelho)
-  csi(1,3)=9.93d03  !resistividade  | 
-  csi(1,4)=3.19d00  !velocidade    _|
-  csi(2,1)=2.53d00  !densidade      | 
-  csi(2,2)=9.35d01  !gama           |===> segunda linha da matriz csi (follhelho)
-  csi(2,3)=9.84d03  !resistividade  | 
-  csi(2,4)=2.87d00  !velocidade    _|
-  csi(3,1)=2.56d00  !densidade      | 
-  csi(3,2)=1.09d02  !gama           |===> terceira linha da matriz csi (folhelho)
-  csi(3,3)=9.28d03  !resistividade  | 
-  csi(3,4)=3.21d00  !velocidade    _|
-  csi(4,1)=2.98d00  !densidade      | 
-  csi(4,2)=3.00d01  !gama           |===> quarta linha da matriz csi (diabásio)
-  csi(4,3)=1.54d08  !resistividade  | 
-  csi(4,4)=5.51d00  !velocidade    _|
-  csi(5,1)=2.93d00  !densidade      | 
-  csi(5,2)=2.80d01  !gama           |===> quinta linha da matriz csi (diabásio)
-  csi(5,3)=1.56d08  !resistividade  | 
-  csi(5,4)=5.20d00  !velocidade    _|
-  csi(6,1)=2.70d00  !densidade      | 
-  csi(6,2)=8.16d00  !gama           |===> sexta linha da matriz csi (dolomita)
-  csi(6,3)=3.54d03  !resistividade  | 
-  csi(6,4)=6.39d00  !velocidade    _|
-  csi(7,1)=2.23d00  !densidade      | 
-  csi(7,2)=8.79d01  !gama           |===> sétima linha da matriz csi (conglomerado)
-  csi(7,3)=5.92d03  !resistividade  | 
-  csi(7,4)=1.97d00  !velocidade    _|
-  csi(8,1)=2.32d00  !densidade      | 
-  csi(8,2)=9.48d01  !gama           |===> oitava linha da matriz csi (cong+emb1)
-  csi(8,3)=3.62d04  !resistividade  | 
-  csi(8,4)=2.06d00  !velocidade    _|
-  csi(9,1)=2.42d00  !densidade      | 
-  csi(9,2)=6.44d01  !gama           |===> nona linha da matriz csi (cong+emb2)
-  csi(9,3)=4.36d05  !resistividade  | 
-  csi(9,4)=2.86d00  !velocidade    _|
-  csi(10,1)=2.60d00  !densidade     | 
-  csi(10,2)=3.97d01  !gama          |===> décima linha da matriz csi (cong+emb3)
-  csi(10,3)=7.42d05  !resistividade | 
-  csi(10,4)=3.71d00  !velocidade   _|
-  csi(11,1)=2.74d00  !densidade     | 
-  csi(11,2)=1.19d01  !gama          |===> décima primeira linha da matriz csi (cong+emb4)
-  csi(11,3)=1.23d06  !resistividade | 
-  csi(11,4)=4.42d00  !velocidade   _|
-  csi(12,1)=2.81d00  !densidade     | 
-  csi(12,2)=7.30d-01  !gama          |===> décima segunda linha da matriz csi (embasamento)
-  csi(12,3)=1.32d06  !resistividade | 
-  csi(12,4)=5.49d00  !velocidade   _|
-
-
-
-
-
+CALL Eclassification(csi,nd)
 
 
 WRITE(*,*)'Sinal de classificação'
@@ -180,8 +125,15 @@ WRITE(*,FMT=13)csi
 
 
 WRITE(*,*)'=============================================='
- CALL classification(csi,omega,rock,evaluation)
 
+ CALL classification(csi,omega,nd,rock,evaluation)
+
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FINAL DO PERCEPTRON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 CALL cpu_time(vf)
 
@@ -200,4 +152,3 @@ CALL cpu_time(vf)
 14 FORMAT(ES12.2)
 
 END PROGRAM perceptron
-
